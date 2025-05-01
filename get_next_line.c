d31141c4a89710c583307517be861de57dfef5e3
Linoa <linoa.judd@protonmail.com>
2025-05-01 15:38:05 +0200

    backbone

---
+++ get_next_line.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljudd <ljudd@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 15:00:37 by ljudd             #+#    #+#             */
+/*   Updated: 2025/05/01 15:37:55 by ljudd            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	(void) fd;
+	return (NULL);
+}
+
+int	main(void)
+{
+	char	*file = "test";
+	int		fd;
+	char	*line;
+
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close (fd);
+}
---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljudd <ljudd@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 15:04:03 by ljudd             #+#    #+#             */
+/*   Updated: 2025/05/01 15:37:05 by ljudd            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE = 10
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+typedef struct s_list_gnl
+{
+	char				*s;
+	struct s_list_gnl	*next;
+}	t_list_gnl;
+
+/* get_next_line.c*/
+char	*get_next_line(int fd);
+
+/* get_next_line_utils.c*/
+
+#endif
